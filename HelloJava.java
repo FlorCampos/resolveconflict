a14de7cf3f2296c4de92193818bf9496e9c5bc6d
Flor Campos Flores <fcfcamposflores@gmail.com>
2022-02-25 20:34:24 -0500

    Message - dev 1

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,10 @@
 public class HelloJava {
+
+	//CAmbios hechos por el dev 1
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hello Java");
+		System.out.println("DEV 1");
 		  
 		  
 	
